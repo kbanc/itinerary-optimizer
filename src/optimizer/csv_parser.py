b63d6db0641c628a96076063ee4daa7b4f3f3b71
kbanc <katherine.bancoft@gmail.com>
2020-05-13 20:38:26 -0400

    csvparser done, tests not complete

--- src/optimizer/csv_parser.py
+++ src/optimizer/csv_parser.py
@@ -1,10 +1,21 @@
 import os
 import csv
 
+from optimizer.location import Location
+
 class CSVParser(object):
 
     @classmethod
     def parse_into_locations(klass, filename):
+        locationList = []
         with open(filename) as data:
-            pass
+            csvReader = csv.reader(data, delimiter=",")
+            csvReader.__next__() #skipping over row of headers
+            for row in csvReader:
+                print("heya I'm called {}.{}".format(row[0], row[1]))
+                if len(row) < 7:
+                    locationList.append(Location(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+                else:
+                    locationList.append(Location(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+        return locationList
 
--- src/test/test_csv_parser.py
+++ src/test/test_csv_parser.py
@@ -5,10 +5,16 @@ from optimizer.csv_parser import CSVParser
 class TestCSVParser(unittest.TestCase):
     
     def test_parse_into_location_takes_filename_arg(self):
-        with patch("builtins.open", mock_open(read_data="data")) as m:
+        with patch("builtins.open", mock_open(read_data="data, beta")):
             CSVParser.parse_into_locations('myfakename')
     
     def test_throws_error_if_file_not_found(self):
         with self.assertRaises(Exception) as context:
             CSVParser.parse_into_locations('myfakename')
         self.assertTrue('No such file' in str(context.exception))
+    
+    # @patch('optimizer.location.Location')
+    # def test_creates_location_object_with_data(self, mock):
+    #     with patch("builtins.open", mock_open(read_data="street,city,country,postalcode,mandatory,role,routeRestriction,nearestIntersection\n\
+    #         123 Sesame St,Fairy,Land,987654,TRUE,car,start,bloor")):
+    #         mock.assert_called()
